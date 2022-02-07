394ecb6890c5518ab5fe807791efeb499d69ed3b
Pedro Belmonte <pedro.belmonte@ing.uchile.cl>
2022-02-07 17:06:57 -0300

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -1,34 +1,43 @@
 from io import BytesIO
+from typing import Optional
 
 from PIL import Image
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Form, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.models import QRInfo
 from app.utils.qr_code import generate_qr_img
 
 
 app = FastAPI(
-    title="QRCodeGenerator",
+    title='QRCodeGenerator',
     description='Given an url and logo (optional), generates a QR code',
-    version="0.0.1",
+    version='0.0.1',
     license_info={
-        "name": "Apache 2.0",
-        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        'name': 'Apache 2.0',
+        'url': 'https://www.apache.org/licenses/LICENSE-2.0.html',
     },
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 
 @app.post(
-    "/generate-qr-code",
-    responses={200: {"content": {"image/png": {}}}},
+    '/generate-qr-code',
+    responses={200: {'content': {'image/png': {}}}},
     response_class=Response,
 )
-async def generate_qr_code(qrinfo: QRInfo) -> Response:
-    logo = qrinfo.logo
+async def generate_qr_code(url: str = Form(...), logo: Optional[UploadFile] = None) -> Response:
     if logo:
         request_object_content = await logo.read()
         logo = Image.open(BytesIO(request_object_content))
 
-    result_qr = generate_qr_img(qrinfo.url, logo)
+    result_qr = generate_qr_img(url, logo)
 
-    return Response(content=result_qr, media_type="image/png")
+    return Response(content=result_qr, media_type='image/png')
